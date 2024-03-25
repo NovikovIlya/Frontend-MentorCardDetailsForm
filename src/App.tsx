72085bbe26c08ce3578f7921ef3c80a75e320cb0
NovikovIlya <novikov.i.sergeevich@gmail.com>
2024-03-25 19:14:15 +0300

    Добавил инпуты

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import "./App.css";
 import { Button, Form, type FormProps, Input, InputNumber } from "antd";
-import styles from './App.module.css'
+import styles from "./App.module.css";
+import { MaskedInput } from "antd-mask-input";
 
 function App() {
   const onFinish: FormProps<any>["onFinish"] = (values) => {
@@ -12,12 +13,15 @@ function App() {
     console.log("Failed:", errorInfo);
   };
 
+  const maskInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+  };
+
   return (
     <div className={styles.container}>
       <Form
         name="basic"
         labelCol={{ span: 8 }}
-      
         className={styles.foorms}
         initialValues={{ remember: true }}
         onFinish={onFinish}
@@ -40,21 +44,43 @@ function App() {
           labelCol={{ span: 24 }}
           label="Card number"
           name="password"
-          rules={[{ required: true, message: "Please input your password!" }]}
+          
+          rules={[
+            {
+              pattern: /^(?:\d{4}-){3}\d{4}$|^\d{16}$/,
+              message:
+                "Пожалуйста, введите корректный номер кредитной карты в формате XXXX-XXXX-XXXX-XXXX",
+            },
+            {
+              required: true,
+              message: "Пожалуйста, введите номер кредитной карты",
+            },
+          ]}
         >
-          <InputNumber style={{ width: "100%" }} />
+          <MaskedInput
+            onChange={maskInput}
+            style={{ width: "100%" }}
+            placeholder="XXXX-XXXX-XXXX-XXXX"
+            mask="0000 0000 0000 0000"
+          />
         </Form.Item>
+
 
         <div className={styles.two}>
           <Form.Item<any>
-            style={{width:'100%'}}
+            style={{ width: "100%" }}
             labelCol={{ span: 24 }}
             label="Exp.date (mm/yy)"
             name="Exp.date"
-            rules={[{ required: true, message: "Please input your password!" }]}
+            rules={[
+              { required: true, message: "Please input your password!" },
+              {},
+            ]}
           >
-            <div className={styles.childs}><InputNumber style={{ width: "50%" }} />
-            <InputNumber style={{ width: "50%" }} /></div>
+            <div className={styles.childs}>
+              <InputNumber type="number" style={{ width: "50%" }} />
+              <InputNumber type="number" style={{ width: "50%" }} />
+            </div>
           </Form.Item>
 
           <Form.Item<any>
@@ -63,12 +89,16 @@ function App() {
             name="CVC"
             rules={[{ required: true, message: "Please input your password!" }]}
           >
-            <InputNumber style={{ width: "100%" }} />
+            <InputNumber type="number" style={{ width: "100%" }} />
           </Form.Item>
         </div>
 
-        <Form.Item >
-          <Button style={{ width: "100%",backgroundColor:'black' }} type="primary" htmlType="submit">
+        <Form.Item>
+          <Button
+            style={{ width: "100%", backgroundColor: "black" }}
+            type="primary"
+            htmlType="submit"
+          >
             Confirm
           </Button>
         </Form.Item>
